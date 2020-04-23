fc7e7598257d9b02782f145e3fd7411fdfd0e794
CrowFea <wendaoliu40@outlook.com>
2020-04-23 15:52:27 +0800

    add model

--- Concentration/Concentration/Concentration.swift
+++ Concentration/Concentration/Concentration.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Concentration {
+    var cards = [Card]()
+    
+    var indexOneAndOnlyFaceUpCard : Int?
+    
+    func chooseCard(at index: Int){
+        if !cards[index].isMatched {
+            if let matchIndex = indexOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUP = true
+                indexOneAndOnlyFaceUpCard = nil
+            }else {
+                //either no cards or two cards face up
+                for filpDownIndex in cards.indices {
+                    cards[filpDownIndex].isFaceUP = false
+                }
+                cards[index].isFaceUP = true
+                indexOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int){
+        for _ in 0..<numberOfPairsOfCards {
+            let card = Card()
+            cards += [card,card]
+        }
+        //TODO:shuffle the cards
+    }
+}
